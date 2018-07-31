090f9a764947327662ad535dea8b887d013a791a
t520-87130\user <teknisk@game.unf.dk>
2018-07-31 16:47:02 +0200

    Added UI

---
+++ Spil/Assets/Scripts/StartMenuBehavior.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+/// <Credit>
+/// Author: Julius 'Bukz' Bendt, Juto Studio
+/// https://wwww.juto.dk
+/// Free-for-all license
+/// </Credit>
+
+public class StartMenuBehavior : MonoBehaviour {
+
+
+    public RectTransform play, credit, exit;
+    public TextMeshProUGUI title;
+    public UIEffects effects;
+
+
+    public Image fader;
+    public Color standardColor;
+
+    public GameObject creditTab,tutorialTab;
+    public RectTransform creditText;
+    public float creditSpeed;
+
+    const float MOVETIME = 0.75f; // how long the MoveRect should take, in sec
+
+    public void Start()
+    {
+        StartCoroutine(effects.TextFade(title,standardColor, 0.3f));
+        StartCoroutine(effects.MoveRect(play, new Vector3(-161,-133, 0), MOVETIME-0.2f));
+        StartCoroutine(effects.MoveRect(credit, new Vector3(161, -133, 0), MOVETIME));
+        StartCoroutine(effects.MoveRect(exit, new Vector3(0, -221, 0), MOVETIME+0.2f));
+    }
+
+ 
+
+    public void Play()
+    {
+        StartCoroutine(effects.ImageFade(fader, standardColor, 0.5f, PlayFaderFinish));
+    }
+
+    public void Credit()
+    {
+        StartCoroutine(effects.ImageFade(fader, standardColor, 1f, OnCreditInFinish));
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    void PlayFaderFinish()
+    {
+        tutorialTab.SetActive(true);
+        StartCoroutine(effects.ImageFade(fader, Color.clear, 0.5f, LoadedTut));
+    }
+
+    void LoadedTut()
+    {
+        FindObjectOfType<tutorial>().StartTutorial();
+    }
+
+    void OnCreditInFinish()
+    {
+        StartCoroutine(effects.ImageFade(fader, Color.clear, 2f));
+        creditTab.SetActive(true);
+        creditText.localPosition = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+        if(creditTab.activeInHierarchy)
+        {
+            creditText.localPosition = new Vector3(0, creditText.localPosition.y + creditSpeed, 0);
+        }
+    }
+
+    public void CloseCredits()
+    {
+        creditTab.SetActive(false);
+    }
+}
---
+++ Spil/Assets/Scripts/UIEffects.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/// <Credit>
+/// Author: Julius 'Bukz' Bendt, Juto Studio
+/// https://wwww.juto.dk
+/// Free-for-all license
+/// </Credit>
+
+public class UIEffects : MonoBehaviour {
+
+
+
+
+    public delegate void OnEventOver();
+
+    public IEnumerator MoveRect(RectTransform rect, Vector3 targetPos,float time)
+    {
+        float timer = 0.0f;
+
+        Vector3 org = rect.transform.localPosition;
+
+
+        while(timer < time)
+        {
+            timer += Time.deltaTime;
+
+            rect.transform.localPosition = Vector3.Lerp(org, targetPos, (timer / time));
+            yield return null;
+        }
+
+        yield return null;
+    }
+
+    public IEnumerator TextFade(TextMeshProUGUI text,Color color, float time)
+    {
+        float timer = 0.0f;
+
+        Color org = text.color;
+
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            text.color = Color.Lerp(org, color, (timer / time));
+            yield return null;
+        }
+    }
+
+    public IEnumerator ImageFade(Image image, Color color, float time,OnEventOver onFinish = null)
+    {
+        float timer = 0.0f;
+
+        Color org = image.color;
+
+        while (timer < time)
+        {
+            timer += Time.deltaTime;
+            image.color = Color.Lerp(org, color, (timer / time));
+            yield return null;
+        }
+
+        if (onFinish != null)
+            onFinish.Invoke();
+    }
+
+    public IEnumerator SmoothTextAdd(TextMeshProUGUI text, string targetText,float time, OnEventOver onFinish = null, bool clearText = true)
+    {
+
+        if (clearText)
+            text.text = "";
+
+        int charPrSec = (int)(targetText.Length / time) +1;
+        int c_index = 0;
+        float timer = 0;
+
+        while(timer < time)
+        {
+            timer += Time.deltaTime;
+            for (int i = 0; i < charPrSec; i++)
+            {
+                if(c_index < targetText.Length)
+                    text.text += targetText[c_index];
+
+
+                c_index++;
+                yield return new WaitForSeconds(Time.deltaTime);
+            }
+            yield return null;
+
+        }
+
+        if (onFinish != null)
+            onFinish.Invoke();
+    }
+
+}
